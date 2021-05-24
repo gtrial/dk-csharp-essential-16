cf046b9bf8d348038f4d84bf30b8ab7e3f7476ed
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-24 17:28:23 +0300

    Task 2 completed

--- task2/Block.cs
+++ task2/Block.cs
@@ -1,7 +1,37 @@
+using System;
+
 namespace task2
 {
     public class Block
     {
-        
+        private int Side1 { get; }
+        private int Side2 { get; }
+        private int Side3 { get; }
+        private int Side4 { get; }
+
+        public Block(int side1, int side2, int side3, int side4)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+            Side4 = side4;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj?.GetType() != GetType()) return false;
+            var other = (Block) obj;
+            return Side1 == other.Side1 && Side2 == other.Side2 && Side3 == other.Side3 && Side4 == other.Side4;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Side1, Side2, Side3, Side4);
+        }
+
+        public override string ToString()
+        {
+            return $"{Side1},{Side2},{Side3},{Side4}";
+        }
     }
 }
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace task2
+﻿using System;
+
+namespace task2
 {
     internal static class Program
     {
         private static void Main()
         {
-            //Создайте класс Block с 4-мя полями сторон, переопределите в нем методы:
-            //Equals – способный сравнивать блоки между собой,
-            //ToString – возвращающий информацию о полях блока.
+            var block1 = new Block(1, 2, 3, 4);
+            var block2 = new Block(1, 2, 3, 4);
+            Console.WriteLine(block1.GetHashCode());
+            Console.WriteLine(block2.GetHashCode());
+            Console.WriteLine(block1.Equals(block2));
+            Console.WriteLine(block1.ToString());
         }
     }
 }
