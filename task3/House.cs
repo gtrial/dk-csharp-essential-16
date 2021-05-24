19abe23391d20545ffcdd3a10da7f051eecb085a
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-24 17:43:25 +0300

    Task 3 completed

--- task3/House.cs
+++ task3/House.cs
@@ -1,7 +1,31 @@
+using System;
+
 namespace task3
 {
-    public class House
+    public class House : ICloneable
     {
-        
+        private double Square { get; }
+        private double Price { get; }
+
+        public House(double square, double price)
+        {
+            Square = square;
+            Price = price;
+        }
+
+        public object Clone()
+        {
+            return (House) MemberwiseClone();
+        }
+
+        public House DeepClone()
+        {
+            return (House) Clone();
+        }
+
+        public override string ToString()
+        {
+            return $"{Square},{Price}";
+        }
     }
 }
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace task3
+﻿using System;
+
+namespace task3
 {
     internal static class Program
     {
         private static void Main()
         {
-            //Создайте класс House c двумя полями и свойствами.
-            //Создайте два метода Clone() и DeepClone(), которые выполняют поверхностное и глубокое копирование соответственно.
-            //Реализуйте простой способ проверки.
+            var house = new House(10, 10000);
+            var house2 = house.Clone();
+            var house3 = house.DeepClone();
+            Console.WriteLine(house.ToString());
+            Console.WriteLine(house2.ToString());
+            Console.WriteLine(house3.ToString());
         }
     }
 }
